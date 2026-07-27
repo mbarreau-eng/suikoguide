a96d3d6150e9a53e1f3477048cf3690bf4a32220
Mathieu Barreau <mathieu.barreau@list.lu>
2026-07-27 11:28:23 +0200

    chap 07

--- data/chapters/ch07.js
+++ data/chapters/ch07.js
@@ -1,5 +1,186 @@
 guideData.chapters.push({
       "id": 7,
       "title": "Lenankamp, plans for a flaming spear",
-      "picture":"7.jpg"
+      "picture":"7.jpg",
+      "party":[
+        {
+          "name":"Hero",
+          "level":"13"
+        },
+        {
+          "name":"Gremio",
+          "level":"10"
+        },
+        {
+          "name":"Viktor",
+          "level":"09"
+        },
+        {
+          "name":"Cleo",
+          "level":"10"
+        }
+      ],
+      "recruits":[
+       {
+          "id": null,
+          "name": "Odessa",
+          "range": "L"
+        }
+      ],
+      "collectibles":["Blueprints"],
+      "paragraphs":[
+        {
+          "type":"plain",
+          "text":"Head straight to the Inn and examine the clock in the back right room. Go talk to Odessa, and she'll be glad to see that you'back; you see, she has some more plans for us. She shows everyone the blueprints to a Fire Spear, because one day, the Liberation Army will be big enough to take on the Imperials; she wants you to take it to her Secret Factory so they can be created. Gremio will want to leave, claiming that they're still members of the Imperial Army, but Odessa tries to convince him that the Empire is evil and corrupt, as we've already seen. You don't want to always be McDohl's little boy, do you?"
+        },
+        {
+          "type":"choices",
+          "items":[
+            "My father goes his way,I go mine.",
+            "Since I'm a member of the Imperial Army..."
+          ]
+        },
+        {
+          "type":"plain",
+          "picture":"7-1.png",
+          "text":"Choose option 1, and Gremio will still try to protest. Cleo goes where you go, so it's decided. We must go northwest of Lenankamp to Mt. Tigerwolf, then to Sarady to meet up with someone from the factory at Sarady. Flik worries that you may be a spy, but Odessa decides that she's going too, so he shouldn't worry. After Odessa joins your party, leave the hideout. As with Ted, don't worry about upgrading her equipment; she's just a temporary member. Leave Lenankamp, and when you're on the World Map, head north, west across a bridge, and north to Mt. Tigerwolf."
+        },
+        {
+          "type":"note",
+          "text":"If you went through Mt. Tigerwolf at the beginning of the game, just ignore the details below, since you should have already gotten the treasure here. Also, the Killer Slimes here drop Water Rune Pieces, which help your character recover HP every round when they're attached to the character's weapon."
+        },
+        {
+          "type":"place",
+          "text":"Mt. Tigerwolf",
+          "party":[
+                {
+                  "name":"Hero",
+                  "level":"13"
+                },
+                {
+                  "name":"Gremio",
+                  "level":"10"
+                },
+                {
+                  "name":"Viktor",
+                  "level":"09"
+                },
+                {
+                  "name":"Cleo",
+                  "level":"10"
+                }
+            ],
+            "savepoints":["Inn"],
+            "bits":[1000],
+            "items":[
+              "Medicine",
+              "Wind Rune Piece",
+              "Antique",
+              "Medicine"
+            ],
+            "equipment":[
+              "Circlet",
+              "Gloves"
+            ],
+            "runes":[
+              "Clone Crystal"
+            ],
+            "enemies":[
+              "Giant Snail",
+              "Killer Slime",
+              "Slasher Rabbit (PS1) / Blade Bunny (Remaster)"
+            ]
+        },
+        {
+          "type":"plain",
+          "text":"Follow the path, then grab the [_CIRCLET_] to your south. Go right to the next screen, follow the path as it goes up stairs, then grab the [_MEDICINE_] before going up to the next area. Move left, down the stairs, then open the chest to the west to find some [_GLOVES_]. Go up the nearby stairs, then right across the bridge and up to the next area. Continue up some more to the next area."
+        },
+        {
+          "type":"plain",
+          "text":"Head left to a fork; to the left is another chest with a [_WIND RUNE PIECE_], so get that, then go up under the bridge. Follow the path as it goes left across the bridge, then up to the next area."
+        },
+        {
+          "type":"plain",
+          "text":"As you go through this area, a man named Ledon stops you and recommends that you rest at his place tonight and continue tomorrow. Odessa wants to keep going, but everyone else thinks that they should rest. You?"
+        },
+        {
+          "type":"choices",
+          "items":[
+            "Me too.",
+            "We'd better go on."
+          ]
+        },
+        {
+          "type":"plain",
+          "text":"Choose either one, because either way you'll decide to stay here anyway. Inside, he'll offer you some tea before dinner, tea that your party finds pretty bitter!"
+        },
+        {
+          "type":"choices",
+          "items":[
+            "Not if it's bitter.",
+            "Just a taste, then."
+          ]
+        },
+        {
+          "type":"plain",
+          "text":"Choose option 2 to move on. Viktor asks what the tea is called, and he says that it's 'Robber's Tea.' At that point, everyone passes out and Ledon will go around and pick your pockets."
+        },
+        {
+          "type":"plain",
+          "text":"Another man named Kessler will come in and see what Ledon is doing. Ledon starts bragging to him about the job he just pulled, but that's when Kessler recognizes Lady Odessa! He'll threaten Ledon and tell him to hurry up and make an antidote or he can say goodbye to his head."
+        },
+        {
+          "type":"plain",
+          "picture":"7-2.png",
+          "text":"After everyone's awake again, Kessler apologizes to Odessa; she just says that she needs to be more careful, so Ledon can keep his head without worrying, for now. The next morning, get out of bed and go talk to both Ledon and Kessler. After his speech about Lady Odessa's ambitions, leave here, then go right and up to the next area."
+        },
+        {
+          "type":"plain",
+          "text":"Head up yet another screen, then move left, up, and right to grab the [_ANTIQUE_] before going up to the next screen. Go up a little, then open the chest to your left for another [_MEDICINE_]. Take the eastern path up and get the [_CLONE CRYSTAL_] out of the chest here, then head back and go up the western stairs. Continue up, but grab the [_1000 BITS_] to your right. Now head north back out to the World Map, then go north and enter Sarady."
+        },
+        {
+          "type":"place",
+          "text":"Sarady",
+          "savePoints":["Inn"]
+        },
+        {
+          "type":"plain",
+          "text":"We're supposed to meet the messenger from the Secret Factory at the Inn, but there's no sign here saying where the Inn is, so go into the building to the right of you when you first enter, and voila! The Inn. Talk to the Innkeeper in here, and choose to stay the night. During the night, you can't sleep, so get up, go outside, and talk to Odessa. She'll want to talk to you about something."
+        },
+        {
+          "type":"choices",
+          "items":[
+            "What is it?",
+            "I'm not interested."
+          ]
+        },
+        {
+          "type":"plain",
+          "text":"Choose 1 and ask her about it. She's worried that she may not be able to live up to everyone else's hopes. She also wonders if you will go back to the Empire."
+        },
+        {
+          "type":"choices",
+          "items":[
+            "Of course.",
+            "I don't understand."
+          ]
+        },
+        {
+          "type":"plain",
+          "picture":"7-3.png",
+          "text":"Choose 2, and she'll lecture you again about the Empire, and how you can judge between right and wrong. She also notices  the compassion that comes from you, and that you have eyes that can draw people together, and if she should ever--"
+        },
+        {
+          "type":"plain",
+          "text":"Odessa will notice someone coming; a ninja named Kage will come and say that he was sent by Mose, chief of the Secret Factory. Odessa asks about a sick pigeon of his, in which Kage says that Mose does not keep any pigeons; that was just a test to make sure Kage was the right person. Odessa will then hand over the blueprints to him, and after that, she'll go back to bed, so go back to one of the empty beds to go back to sleep."
+        },
+        {
+          "type":"plain",
+          "text":"After a little morning chatter, you'll regain control. Leave the Inn and, if you need supplies, stop at the Item Shop. After you're done, leave Sarady and head back to Mt. Tigerwolf. I'm sure you don't need directions on how to get back to the other side, so make your way through the mountain and back to the World Map, then go back to Lenankamp."
+        },
+        {
+          "type":"note",
+          "text":"Before entering Lenankamp, TAKE EVERYTHING OFF OF ODESSA."
+        }
+      ]
     });
