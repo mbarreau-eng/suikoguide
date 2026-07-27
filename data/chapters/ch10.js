30ad4e82e5d03a17f8736c93491695031ee56f23
Mathieu Barreau <mathieu.barreau@list.lu>
2026-07-27 13:10:57 +0200

    chap 10

--- data/chapters/ch10.js
+++ data/chapters/ch10.js
@@ -1,4 +1,66 @@
 guideData.chapters.push({
       "id": 10,
-      "title": "Seika, a new leader is born"
+      "title": "Seika, a new leader is born",
+      "picture":"10.jpg",
+      "recruits":[{"id":5}],
+      "party":[
+        {"name":"Hero","level":"15"},
+        {"name":"Gremio","level":"14"},
+        {"name":"Viktor","level":"14"},
+        {"name":"Cleo","level":"14"}
+      ],
+      "savepoints":["Inn"],
+      "enemies":["Empire Soldier (PS1) / Imperial Soldier (Remaster) (sword #1)", "Empire Soldier (PS1) / Imperial Soldier (Remaster) (spear #1)"],
+      "paragraphs":[
+        {
+          "type":"plain",
+          "picture":"10-1.png",
+          "text":"Go to the upper right of town and talk to the man in brown near the stairs. He'll tell you that Mathiu's house is just up those stairs. Go up the stairs, into the house, and talk to all three of Mathiu's students. Once you've done that, go back outside and talk to the man in brown again, who is in fact Mathiu. Gremio will show him the earring that Odessa gave to them, telling him that it was her last request. Mathiu doesn't approve of what Odessa was doing, so Viktor will ask him what was wrong with what she was doing. In any case, Mathiu refuses the earring, tells us to leave, and heads back into his house."
+        },
+        {
+          "type":"plain",
+          "text":"Follow him into his house and pester him some more; he still tells us that he doesn't want anything to do with Odessa. Leave his  ouse again, and some Imperial Soldiers will arrive. They'll run right past you into Mathiu's house, so go inside and see what the deal is."
+        },
+        {
+          "type":"plain",
+          "text":"The soldiers will be giving him the order to return to the Imperial Army for duty. Mathiu will refuse them, saying he wants nothing to do with war. Of course, they decide that they will take him by force if they have to; they even stoop low enough to giving him the  ultimatum of choosing to return to duty or to let one of his students... well, there's no telling what could happen to that child. Now, what do we usually do right now?"
+        },
+        {
+          "type":"choices",
+          "items":["Help them, of course.","Wait. Let's see what's happening."]
+        },
+        {
+          "type":"choices",
+          "items":["Survivors of the Rebel Army.","Just some good guys passing by."]
+        },
+        {
+          "type":"plain",
+          "picture":"10-2.jpg",
+          "text":"Choose 1 to help Mathiu out. The soldiers will wonder who you are; choosing option 2 in the next set of choices makes you seem cooler, but either way, you will fight a  group of Empire Soldiers. After the battle, go inside and talk to Mathiu again. He knows who you are, and Gremio will point out to him that the soldiers called him Mathiu Silverberg; Odessa was his sister. He decided that he wanted nothing to do with war anymore, but Odessa kept fighting for what she believed in. When he asks how she described him, Cleo tells him that she said 'just a man in Seika named Mathiu', meaning that she never forgave him."
+        },
+        {
+          "type":"plain",
+          "text":"He was a coward; he wouldn't fight for what he believed in because of what war did to him, seeing people die due to his actions, but now, he decides to strive for what Odessa was striving for. When Gremio tries to give him the earring again, he says that you should keep it."
+        },
+        {
+          "type":"choices",
+          "items":["Why?","But Odessa..."]
+        },
+        {
+          "type":"plain",
+          "text":"If you ask him why, he'll tell you that there's an inscription on it with the location of the Liberation Army's hideout, and whoever holds this earring will lead the Freedom Fighters. He will tell you that he would be unable to lead the Liberation Army, but you would be, since you're a born leader. Do you accept?"
+        },
+        {
+          "type":"choices",
+          "items":["I can't do it.","I'll do it."]
+        },
+        {
+          "type":"plain",
+          "text":"Of course! Choose 2 to become the new leader of the Liberation Army. Cleo and Gremio decide to stay with  you, even if it means never going back to the Empire. Mathiu says that we need to get a Headquarters and to recruit some volunteers. The place would have to be big enough to withstand an attack by the Imperial Army; he says that the abandoned castle in the middle of Lake Toran would be perfect."
+        },
+        {
+          "type":"plain",
+          "text":"First, you need to go to Kaku so we can get a ride to the castle. Mathiu joins your party, but he can't go just yet because he has to get ready, as well as give the students plenty of homework. Shop around here if you need to (you can get a Water Crystal for Gremio if you really want to, but I'll keep the Prosperity Rune on him for a while), then leave Seika. Head southwest to Kaku."
+        }
+      ]
     });
