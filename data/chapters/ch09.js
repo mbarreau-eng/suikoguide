442f8f12298909079d06b57ee4faa5beb0030e9f
Mathieu Barreau <mathieu.barreau@list.lu>
2026-07-27 12:46:28 +0200

    chap 09

--- data/chapters/ch09.js
+++ data/chapters/ch09.js
@@ -1,4 +1,58 @@
 guideData.chapters.push({
       "id": 9,
-      "title": "Kwaba, the Fortress of Lord Ain Gide"
+      "title": "Kwaba, the Fortress of Lord Ain Gide",
+      "picture":"9.jpg",
+      "party":[
+        {"name":"Hero","level":"14"},
+        {"name":"Gremio","level":"12"},
+        {"name":"Viktor","level":"12"},
+        {"name":"Cleo","level":"12"}
+      ],
+      "paragraphs":[
+        {
+          "type":"plain",
+          "text":"Cleo says that Lord Ain Gide, a friend of Teo's, is in charge here; he knows your face, but not anyone else's here. Viktor will decide to come up with fake names in case, so Gremio will be Roi, Cleo will be Maria, and Viktor will be Schtolteheim Reinbach III (haha, play all of the Suikodens, and you'll know why I'm laughing). As for you..."
+        },
+        {
+          "type":"choices",
+          "items":["Masamune.","Piisuke.","Schtolteheim Reinbach IV."]
+        },
+        {
+          "type":"plain",
+          "text":"Pick whichever you like, then head south  to be stopped by the guards. They will notice that you look like the McDohl kid, and that's when Ain Gide himself will show up (uh oh). He'll demand to see your face, and that will be when Gremio will charge you and start screaming at you for always causing trouble and being useless and everything else. He'll also offer to lop off your head!"
+        },
+        {
+          "type":"plain",
+          "text":"Ain Gide will decide to let you pass, since McDohl's son wouldn't be dressed like a beggar. As you pass by him, he'll mention to you to take good care of your father... (Yeah, he knows...)"
+        },
+        {
+          "type":"plain",
+          "text":"On the other side, Gremio apologizes for his behavior back there, and will even let you kill him if you think that's what needs done..."
+        },
+        {
+          "type":"choices",
+          "items":["Unforgivable!","I understand, Gremio."]
+        },
+        {
+          "type":"plain",
+          "picture":"9-1.png",
+          "text":"Choose 2 to forgive him, since he was just trying to help. Talk to Chandler on the left;he wants to open his own store one day, so remember that. Head south out of Kwaba."
+        },
+        {
+          "type": "place",
+          "text": "World Map",
+          "places": [
+            "S. Kwaba","Seika","Kaku","Kouan","W. Great Forest","E. Garan Area"
+          ],
+          "enemies":["Beast Commander","Flying Squirrel","Killer Rabbit (PS1) / Killer Bunny (Remaster)","Roc"]
+        },
+        {
+          "type":"plain",
+          "text":"Feel free to walk around and fight if you want to level up a couple of times, but when you're ready, make your way south to Seika."
+        },
+        {
+          "type":"note",
+          "text":"You might be able to get a Phero Crystal from the Beast Commander. Those babies sell for 25,000 Bits!"
+        }
+      ]
     });
