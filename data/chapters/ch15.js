54f35dfb59ba2945e80e9d4abcfe1d2ca58d88fc
Mathieu Barreau <mathieu.barreau@list.lu>
2026-07-28 13:14:05 +0200

    chapt 15

--- data/chapters/ch15.js
+++ data/chapters/ch15.js
@@ -1,4 +1,85 @@
 guideData.chapters.push( {
       "id": 15,
-      "title": "Village of the Elves, will everything turn to ashes?"
+      "title": "Village of the Elves, will everything turn to ashes?",
+      "picture":"15/15.png",
+      "savepoints":["Tree base","Inn"],
+      "recruits":[{"id":32}],
+      "party":[
+        {"name":"Hero","level":"23"},
+        {"name":"Kai","level":"23"},
+        {"name":"Pahn","level":"22"},
+        {"name":"Gremio","level":"23"},
+        {"name":"Cleo","level":"23"},
+        {"name":"Kirkis","level":"22"}
+      ],
+      "paragraphs":[
+        {
+          "type":"plain",
+          "picture":"15/15-1.png",
+          "text":"Use the Journeyman's Crystal if you want to, then head up into the tree. You'll automatically go up to find one of the Empire's generals, General Valeria; she's trying to persuade them that Kwanda is planning on burning down the entire forest, not just the Village, and this forest is her home as well. They'll decide not to listen to her and lock her up."
+        },
+        {
+          "type":"plain",
+          "picture":"15/15-2.png",
+          "text":"Take this opportunity to visit the Armory, and once you're done there, go north and into the house here. You'll meet Sylvina, Kirkis's girlfriend; she was worried that Kirkis left without her. Talk to the elder here and he'll wonder why Kirkis left; when Kirkis tells him the story, he claims that he doesn't need human help to defend the village and will decide to lock everyone up."
+        },
+        {
+          "type":"plain",
+          "picture":"15/15-3.png",
+          "text":"Talk to Stallion down here if you want to, then talk to Valeria. She'll tell you that the elves wouldn't listen to her, and then she'll tell you Kwanda's plans; he's going to use an object called the Burning Mirror to burn down the entire forest."
+        },
+        {
+          "type":"plain",
+          "picture":"15/15-4.png",
+          "text":"Examine both sets of bars and you'll hear Sylvina come down to the prison. She'll want to know why Kirkis keeps trying to help the humans; he says that he doesn't understand why humans, dwarves, and elves just can't get along, so he wants to help. She decides that she'll believe him, as well as decide to let you out of prison. Kirkis has an idea to stop the Burning Mirror, and that's to go to the dwarves and ask for their help."
+        },
+        {
+          "type":"choices",
+          "items":["I agree.","Let's go back to the castle first."]
+        },
+        {
+          "type":"plain",
+          "picture":"15/15-5.png",
+          "text":"Choose either one, because we don't have much time so we're going the to Village of the Dwarves now anyway. When you gain control, leave the prison and once you're back out at the village, make sure to stop at the Item Shop and Armory if you haven't yet, then head back down to the trunk of the tree. Valeria will then ask to join your party so she can help protect the forest, meaning that you have to boot someone out of your party."
+        },
+        {
+          "type":"plain",
+          "text":"Kirkis will ask Sylvina to join too, but she says that she will wait for now, and will wait for him to come back. Back out on the World Map, head east and north a little to the Dwarf Trail."
+        },
+        {
+          "type":"place",
+          "text":"Dwarf Trail",
+          "party":[
+            {"name":"Kai","level":"23"},
+            {"name":"Valeria","level":"26"},
+            {"name":"Pahn","level":"22"},
+            {"name":"Hero","level":"23"},
+            {"name":"Gremio","level":"23"},
+            {"name":"Kirkis","level":"22"}
+          ],
+          "savepoints":["Close to the exit"],
+          "items":["Sacrificial Buddha","Medicine","Fortune Rune Piece"],
+          "equipment":["Feather","Karate Uniform","Guard Robe"],
+          "runes":["Killer Crystal"],
+          "enemies":["Death Boar (PS1) / Wild Boar (Remaster)","Dwarf (PS1) / Stray Dwarf (Remaster)","Eagle Man"]
+        },
+        {
+          "type":"note",
+          "text":"The Death Boar can drop Boar Crystals, which can be attached to three characters in the game, which includes Pahn, who has one already equipped, and two others."
+        },
+        {
+          "type":"plain",
+          "picture":"15/15-6.png",
+          "text":"Head east and north to the next screen, then go northwest behind the tree on your left to a chest containing a [_FEATHER_]. Get back on the trail and follow the path to the next area. Go up, and when you see the chest to your right, go right behind a tree and straight to it to get a [_KARATE UNIFORM_]. At the fork just above the chest, take the left path first to find a [_SACRIFICIAL BUDDHA_]."
+        },
+        {
+          "type":"plain",
+          "text":"Go back to the fork and head right and up to the next screen. Follow the path to another fork; go up the first ladder on the left to come across a chest with a [_GUARD ROBE_] in it, then head back down and move east to find another hidden chest with a [_KILLER CRYSTAL_] inside. Go up the second ladder here, then follow the path to the next screen. Look for a chest to your south to find a [_MEDICINE_], then head up to the next area."
+        },
+        {
+          "type":"plain",
+          "picture":"15/15-7.png",
+          "text":"Grab the [_FORTUNE RUNE PIECE_] out of the chest to the north, then go east under the grating and up the little stairs, following the path to the next screen. You'll meet up with Kuromimi again, mumbling about the stingy dwarves not giving him the cure for his sick friends. He'll run off again, so just head up to the next screen where you'll find a Journeyman's Crystal. Use it, then follow the path back out to the World Map and go northeast to the Village of the Dwarves."
+        }
+      ]
     });
