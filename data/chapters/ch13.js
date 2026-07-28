f5799b92ef3eb0f5244caadbd5b574f3d94cc31f
Mathieu Barreau <mathieu.barreau@list.lu>
2026-07-28 11:52:54 +0200

    chap 13

--- data/chapters/ch13.js
+++ data/chapters/ch13.js
@@ -1,4 +1,230 @@
 guideData.chapters.push({
       "id": 13,
-      "title": "Kouan, the theft of the great Kirinji"
+      "title": "Kouan, the theft of the great Kirinji",
+      "picture":"13/13.png",
+      "party":[
+        {"name":"Hero", "level":"21"},
+        {"name":"Kai", "level":"22"},
+        {"name":"Viktor", "level":"21"},
+        {"name":"Cleo", "level":"21"},
+        {"name":"Gremio", "level":"21"},
+        {"name":"Luc", "level":"21"}
+      ],
+      "savepoints":["Inn"],
+      "bits":[700],
+      "items":["Medicine","Speed Rune Piece","Fire Rune Piece","Medicine","Water Rune Piece","Antique"],
+      "Equipement":["Shoulder Pads"],
+      "runes":["Counter Crystal"],
+      "collectibles":["Kirinji"],
+      "recruits":[{"id":18},{"id":19},{"id":20},{"id":21},{"id":22},{"id":23},{"id":24}],
+      "enemies":["Empire Soldier (PS1) / Imperial Soldier (Remaster) (sword #1)","Robot Soldier (spear)","Robot Soldier (sword)"],
+      "paragraphs":[
+        {
+          "type":"plain",
+          "picture":"13/13-1.png",
+          "text":"Go to the mansion to your upper left and talk to Giovanni; he'll tell you that Lepant is too busy to see you, and when Viktor asks when he'll be free, he says that it could be anywhere from a day to a year, so we've got to find another way to talk to him."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-2.png",
+          "text":"Go to the Inn next. Inside is Lorelai, but she won't join your party until the Hero is at level 25, so it won't be too much longer until then. Anyway, talk to the man sitting in the chair, Krin. He seems a little sneaky, but he claims that he has a plan to get into Lepant's mansion."
+        },
+        {
+          "type":"choices",
+          "items":["Tell me what you have in mind.","I can't trust you."]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-3.png",
+          "text":"Choose option 1, and he'll drag you over to the Inn part of the building. He'll tell you that Lepant doesn't want to get involved with anyone who defies the Empire, so he suggests another way of getting you to see him; stealing his sacred sword, Kirinji. Krin will go and set up a way to get into his house to steal Kirinji, so after a while, you'll gain control."
+        }  ,
+        {
+          "type":"plain",
+          "picture":"13/13-4.png",
+          "text":"Stay at the Inn to recover your MP if you need to, and also save before you leave, then go over to the right side of his mansion. Krin will fix a rope to the tree so we can sneak in through the roof. Once on the roof, head left and down the ladder. Head right, down, and left to find some [_SHOULDER PADS_] in a chest, then go down to the next screen."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-5.png",
+          "text":"The Robot Soldiers in this place are tough, but imagine how much harder it would be without the Master Pupil attack and Kai! Head down the ladder and go through the door. Go down into another door and talk to the man in here; this is Rock, and he's a warehouse guard and he's likes to build vaults as well. When he finds out that you don't have a vault, he'll want to join you so he can build one. Recruit him, then grab the [_MEDICINE_] and [_700 BITS_] out of the chests in here."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-6.png",
+          "text":"Go back out in the hallway and go through the door on the right to be in a room full of Robot Soldiers. Here's the trick; you have to walk BEHIND all of them; do not walk in their field of vision or you'll have to start over. So, walk under the first two, then go up and zigzag behind the two above them, then go right and down to a chest containing a [_COUNTER CRYSTAL_]. Go back to the top and through the door."
+        },
+        {
+          "type":"plain",
+          "text":"Move right at the fork and go through the door to find a chest with a [_SPEED RUNE PIECE_]. Go down the hallway and down the stairs, then left and up to a fork. The room above you has a [_LEATHER ARMOR_], the room to the right of that one has a [_FIRE RUNE PIECE_], then there are two rooms to the left; the first has a [_MEDICINE_], and the second has the [_WHITE PAINT_] in it."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-7.png",
+          "text":"Talk to the man beside the third door to find out that this is Juppo, he's an inventor and a Trickster; he invented all of the dolls that we've been fighting, as well as 'the big one' that we'll see in the next room. He'll then leave. Go into the next room to the left."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-8.png",
+          "text":"WOW! This giant wheel just isn't any fun. There's no way to control it; it'll just plop you down wherever it pleases, which usually means fighting a bunch of monsters, losing some EXP, and maybe a treasure or two before it lets you off on the other side. Once you've passed it, head north through the two doors to find the Lepant's treasure, [_KIRINJI_]. Head all the way back to the roof and get off on the right side."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-9.png",
+          "text":"Back at the Inn, Krin will be glad that you made it back, and he will offer you some tea. Wait, tea? Didn't some other sneaky guy try to give us tea before? After everyone refusing it and putting Krin to bed, you'll hear Lepant all fired up about someone stealing Kirinji; as it turns out, Krin told Lepant about it so he would catch you in your sleep. Lepant will come in and demand to know why you took Kirinji."
+        },
+        {
+          "type":"choices",
+          "items":[".........","Um, Mathiu recommended you."]
+        },
+        {
+          "type":"plain",
+          "text":"Choose 2 to tell him what's going on. He says that he'd like to help, but there are certain things that he cannot give up."
+        },
+        {
+          "type":"choices",
+          "items":["We need your help.","I understand. We'll return your sword."]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-10.png",
+          "text":"Choose 2 to say that you understand so you can give his sword back to him. Giovanni will come running in, saying that Lepant's wife, Eileen, was taken away by the new military commander. He'll rush out, so leave the Inn and head up to the mansion at the upper right of town to find Lepant demanding why Eileen was taken. It turns out that the new commander is Kraze! Lepant will take out the two guards, so follow him into the mansion."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-11.png",
+          "text":"Inside, he'll take two more of them out, but don't follow him again. Go to the room on the lower left and grab the [_WATER RUNE PIECE_] from inside the chest. Go back out and go into the room Lepant went in. He'll be surrounded, so he'll join you to fight them. You have to get take someone out of your party for the time being while he's there (sorry, Luc)."
+        },
+        {
+          "type":"plain",
+          "text":"Lepant will be in the back row anyway, where he can't do a thing, but as long as Kai is in your party, the battle will be no problem. After the battle, stick Lepant in the front row and head up the stairs. Go down through the door, then right and through this door. Take the door on the left first and get the [_ANTIQUE_] out of the chest, then leave and go into the room on the right."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-12.png",
+          "text":"Kraze will be surprised to see you, since it's your fault that he was reassigned to this country town, but he won't let him ruin his pleasure (ew, gross). You'll be attacked by 6 more Empire Soldiers, so take them out as well. After that, he'll take Eileen hostage, as well as summon an old friend to take you out; Pahn! He says he doesn't regret the decision he made that day, but his heart still aches about it, so he'll run over and attack Kraze! You will then be given a choice. Kill Kraze or let him live."
+        },
+        {
+          "type":"choices",
+          "items":["You're the one person I can't forgive.","Get the hell out of here."]
+        },
+        {
+          "type":"plain",
+          "text":"Surprisingly enough, you can pick either option without consequences! This is the one time you can get away with killing someone, so if you want to, go ahead because even if you don't, you won't be seeing him again, so pick 1 to kill and 2 to not kill."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-13.png",
+          "text":"Talk to Lepant, and he'll tell you that, since he's a fugitive now, he will join your army! He tries to break it off with Eileen, seeing as how he won't be able to come back, but she won't hear of it; she'll go with him! Now, talk to Pahn, and he'll apologize to you. He also wants to join the Liberation Army, even if you don't forgive him."
+        },
+        {
+          "type":"choices",
+          "items":["I can't forgive you.","Pahn, lend us a hand."]
+        },
+        {
+          "type":"plain",
+          "text":"C'mon now, we're good guys. Choose 2 to forgive him and let him join the army too. Here's all the people who just joined the Liberation Army; Rock, Lepant, Eileen, Pahn, Giovanni, Krin, and Juppo too! As soon as you are done, head back to Kaku, get on your boat, and go back to Headquarters."
+        },
+        {
+          "type":"place",
+          "text":"Headquarters",
+          "party":[
+            {"name":"Hero","level":"21"},
+            {"name":"Kai","level":"22"},
+            {"name":"Viktor","level":"21"},
+            {"name":"Cleo","level":"21"},
+            {"name":"Gremio","level":"21"}
+          ],
+          "recruits":[{"id":25},{"id":26},{"id":27}]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-14.png",
+          "text":"Head to the 1st floor first, and guess who's here? It's our good buddies Varkas and Sydonia! They figured they needed to pay you back for saving them, so they're going to join the Liberation Army as well! They would also like to say hello to Odessa, and that's when Mathiu comes in and tells them that she went north on a mission. Mathiu then suggests that you hold a banquet to welcome all of our new friends!"
+        },
+        {
+          "type":"choices",
+          "items":["Let's do it.","Now wait a..."]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-15.png",
+          "text":"Choose option 1, and let the feasting begin! At the banquet, talk to all 10 people in here, then leave the room. When you do, an assassin runs in to attack you and take that rune off your right hand! In the battle, Deadly Fingertips won't work on him, so just attack him for a few rounds until he disappears and leaves the castle, saying that Lady Windy is always ready to destroy you."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-16.png",
+          "text":"Pahn and Gremio will be arguing over who's going to stand watch over you, so all you have to do is crawl into bed. The next morning, Gremio will come in and join you, and you'll gain control. Head down to the basement, then make your way out to the boat to find an elf named Kirkis passed out beside it."
+        },
+        {
+          "type":"plain",
+          "text":"Back in one of the beds, Kirkis comes to and asks if this is the Liberation Army HQ. He'll want to speak to the leader, Odessa, but of course, Mathiu will tell him that you're the leader while she's away. In any case, Kirkis will ask you to save the elves, because the great General Kwanda Rosman is planning on exterminating all of them."
+        },
+        {
+          "type":"choices",
+          "items":["Of course.","But..."]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-17.png",
+          "text":"Pick either option, and Mathiu will tell you that we only have a few hundred men, not nearly enough to mobilize their forces, but he will send a team to the Village of the elves to check it out, meaning you, Gremio, Kirkis, and three others. Just for the sake of recruiting others,pick Tai Ho, Lepant, and Juppo. This is just temporary, though."
+        },
+        {
+          "type":"plain",
+          "text":"Before doing anything else, visit Rock at the Vault, and put all of your important items in there, including the White Paint and any valuable Crystals you aren't going to use just yet."
+        },
+        {
+          "type":"plain",
+          "text":"Before going to the Village of the Elves, there are three more people we can recruit. First stop is Kaku for two of them."
+        },
+        {
+          "type":"place",
+          "text":"Kaku",
+          "party":[
+            {"name":"Hero","level":"22"},
+            {"name":"Gremio","level":"22"},
+            {"name":"Lepant","level":"19"},
+            {"name":"Tai Ho","level":"22"},
+            {"name":"Kirkis","level":"15"},
+            {"name":"Juppo","level":"10"}
+          ],
+          "recruits":[{"id":28},{"id":29}]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-18.png",
+          "text":"Head to the upper right of town and talk to the little girl standing beside the Inn; this is Meg, and she's Juppo's niece. She's been looking for him for a long time so she could learn some new tricks from him. He is reluctant to let her join, but then she'll ask you herself to join. Choose to recruit her, then go into the Inn and SAVE YOUR GAME. You may need to reset up ahead."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-19.png",
+          "text":"Head to the Tavern and go to the basement where you recruited Tai Ho. Down here is a man named Gaspar; Tai Ho was one of his best customers, and since Tai Ho joined the Liberation Army, he wants to as well, but only if you win 5,000 Bits from him at Dice, which is why you needed to save before coming here. After you've won the money, recruit Gaspar, then leave Kaku and head north to Seika."
+        },
+        {
+          "type":"place",
+          "text":"Seika",
+           "party":[
+            {"name":"Hero","level":"22"},
+            {"name":"Gremio","level":"22"},
+            {"name":"Lepant","level":"19"},
+            {"name":"Tai Ho","level":"22"},
+            {"name":"Kirkis","level":"15"},
+            {"name":"Juppo","level":"10"}
+          ],
+          "recruits":[{"id":30}]
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-20.png",
+          "text":"Go to the Inn and talk to the guy on the left beside the table. He's Sheena, and he'll try to start smooth-talking the girl here, but that's when Lepant comes out and yells at him. As it turns out, Sheena's his son, so he will grab him and drag him to Headquarters. That's it for new recruits, so head back to your castle. If you've recruited everyone possible so far, you will see a quick scene showing that your castle has grown. Take time to tour your castle again to see where everything is."
+        },
+        {
+          "type":"plain",
+          "picture":"13/13-21.png",
+          "text":"Go to the third floor to talk to Mathiu and put whomever you want in place of Lepant, Juppo, and Tai Ho. Head back to Kaku, then leave there and head far south-southeast to the Great Forest. Stop at Kouan if you need to update some of your armor."
+        },
+        {
+          "type":"note",
+          "text":"Now that Pahn is back in your party, I STRONGLY RECOMMEND keeping him in your active party at all times. He will eventually take part in a battle all by himself, and he will need all of the experience he can get, and the outcome of the battle will determine whether you get all 108 Stars of Destiny or not."
+        }
+      ]
     });
