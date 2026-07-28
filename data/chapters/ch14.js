8f722afaff3672e4e322a0b24da0664ab5a66642
Mathieu Barreau <mathieu.barreau@list.lu>
2026-07-28 12:37:43 +0200

    chapt 14

--- data/chapters/ch14.js
+++ data/chapters/ch14.js
@@ -1,4 +1,42 @@
 guideData.chapters.push({
       "id": 14,
-      "title": "Great Forest, the path to different cultures"
+      "title": "Great Forest, the path to different cultures",
+      "picture":"14/14.png",
+      "recruits":[{"id":31}],
+      "savepoints":["Inn"],
+      "items":["Escape Talisman","Needle","Water Rune Piece","Fortune Rune Piece"],
+      "equipement":["Gauntlet"],
+      "enemies":["Holly Boy","Holly Spirit (PS1) / Holly Elf (Remaster)","Kobold (bow)","Kobold (sword)"],
+      "paragraphs":[
+        {
+          "type":"plain",
+          "picture":"14/14-1.png",
+          "text":"When you enter the Great Forest, a man will tell you that he's been seeing elves running around, then he'll spot Kirkis and run off to tell everyone. Head north and east, then enter the actual forest. As you go east, Kirkis will tell you that without him you can't go through this area since it's under a spell, which explains why the screen kept repeating if you tried to earlier."
+        },
+        {
+          "type":"plain",
+          "picture":"14/14-2.png",
+          "text":"On the next screen, head up and right to the next area. Cross the log bridge and a girl named Viki will mysteriously pop up behind you. She seems lost and wants you to protect her, so go ahead and recruit her and she'll teleport herself to Headquarters."
+        },
+        {
+          "type":"plain",
+          "picture":"14/14-3.png",
+          "text":"Anyway, head east but don't go down the stairs; keep going east and open the chest on the next screen to find a [_GAUNTLET_]. Go back and head down the stairs this time, then go right to the next screen to find another chest; this one has an [_ESCAPE TALISMAN_] in it. Head back and head left across the log to the next screen, where you'll find another chest; this one has a [_NEEDLE_] inside. Go down from there and open the semi-hidden chest for another [_WATER RUNE PIECE_]. Move right two screens, grab the [_FORTUNE RUNE PIECE_] to your  north, then continue right some more, following the path until you end up in a village."
+        },
+        {
+          "type":"plain",
+          "picture":"14/14-4.png",
+          "text":"This is the Kobold Village, but Kirkis says that all of the Kobolds are gone. At that time, a Kobold named Kuromimi will come up to you, saying that you humans took away all of his friends, but also that they're sick. We have to figure this out too, but the Village of the Elves is first. Leave here."
+        },
+        {
+          "type":"place",
+          "text":"World Map",
+          "places":["Kobold Village","Village of the Elves","W. Dwarf Trail","Pannu Yakuta","E. Dwarf Trail","Village of the Dwarves","Dwarves' Vault Areas"],
+          "enemies":["Kobold (bow)","Kobold (sword)","Kobold (mage)","Strong Arm"]
+        },
+        {
+          "type":"plain",
+          "text":"Head southeast of the Kobold Village until you go through a path between the trees. To your southwest is a different looking tree; that's the Village of the Elves."
+        }
+      ]
     });
