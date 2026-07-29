0f50836ca97df138329626636aa5dfae0dacacdb
mbarreau-eng <mbarreau@gmail.com>
2026-07-29 14:46:13 +0200

    chapt 19

--- data/chapters/ch19.js
+++ data/chapters/ch19.js
@@ -2,7 +2,7 @@ guideData.chapters.push({
       "id": 19,
       "title": "Teien, looking for an antidote",
       "picture":"19/19.png",
-      "collectibles":["Engine"],
+      "collectibles":["Engine","Mathiu's Letter","Fake Orders"],
       "party":[
         {"name":"Pahn","level":"28"},
         {"name":"Viktor","level":"32"},
@@ -25,6 +25,117 @@ guideData.chapters.push({
         {
           "type":"choices",
           "items":["We have no business with you.","We need a boat that can beat the rapids."]
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-2.png",
+          "text":"Choose 2, and he will decide to help you, but he can't do it alone; he needsthe help of the old man next door, Kamandol. Gen will join your party, so you have to kick someone out of it temporarily. Leave his house and go next door to have Kamandol come out to talk to you; Gen will make fun of him for being so old, but Kamandol says that he can do anything, depending on the time and money."
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-3.png",
+          "text":"Go inside his house, and he'll show you a machine that moves by burning oil; by attaching this to the boat, we can navigate the Dunan Rapids. He tells us to be careful with it, meaning that... we have to carry it? Kamandol will join your party, thus kicking out your other party member. You'll obtain the engine, which is listed as the [_Engine_] in your Party Items. Leave Teien and head south to Rikon."
+        },
+        {
+          "type":"place",
+          "text":"Rikon",
+          "party":[
+            {"name":"Viktor","level":"32"},
+            {"name":"Flik","level":"35"},
+            {"name":"Gen","level":"23"},
+            {"name":"Hero","level":"33"},
+            {"name":"Gremio","level":"33"},
+            {"name":"Kamandol","level":"22"}
+          ]
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-4.png",
+          "text":"You'll automatically drop it off at the south end of town, so the two of them will start to work on it while we get some rest. In the middle of the night, Gremio will check up on you, saying that he doesn't think that you need him to protect you anymore..."
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-5.png",
+          "text":"The next morning, leave the Inn and you'll go talk to them; they worked throughout the night and completed it, so they'll rejoin you. Now, hop on the boat and follow the river to the blinking dot to your south to arrive at Liukan's Hermitage."
+        },
+        {
+          "type":"place",
+          "text":"Liukan's Hermitage",
+          "party":[
+            {"name":"Viktor","level":"32"},
+            {"name":"Flik","level":"35"},
+            {"name":"Gen","level":"23"},
+            {"name":"Hero","level":"33"},
+            {"name":"Gremio","level":"33"},
+            {"name":"Kamandol","level":"22"}
+          ],
+          "collectibles":["Black Paint"]
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-6.png",
+          "text":"Head up the stairs on the right and talk to Liukan; he says that he moved here to try to get away from everyone, and from making medicine. When Gremio mentions that we need him to make an antidote for Milich's roses, Milich will come flying in on a dragon; after some very strong (*grin*) words from Milich, he will decide to take Liukan to the impenetrable Soniere Prison."
+        },
+        {
+          "type":"plain",
+          "text":"Head into his house and grab the [_BLACK PAINT_] out of the chest. Now we need to head back to Headquarters, so get on your boat, then use your Blinking Mirror to head back."
+        },
+        {
+          "type":"place",
+          "text":"Headquarters",
+          "party":[
+            {"name":"Viktor","level":"32"},
+            {"name":"Flik","level":"35"},
+            {"name":"Gen","level":"23"},
+            {"name":"Hero","level":"33"},
+            {"name":"Gremio","level":"33"},
+            {"name":"Kamandol","level":"22"}
+          ]
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-7.png",
+          "text":"Head up to the fourth floor and talk to Mathiu. He has an idea; he knows a counterfeiter in Antei named Kimberly, as well as a scrivener named Tesla. He wrote you a letter to give to Kimberly, but he won't tell us his idea just yet. You'll receive  [_MATHIU'S LETTER_]. Talk to Sanchez to put the two people back in your party that you want, then head over to Antei."
+        },
+        {
+          "type":"place",
+          "text":"Antei",
+          "party":[
+            {"name":"Pahn","level":"28"},
+            {"name":"Viktor","level":"32"},
+            {"name":"Flik","level":"35"},
+            {"name":"Hero","level":"33"},
+            {"name":"Gremio","level":"33"},
+            {"name":"Kai","level":"28"}
+          ],
+          "recruits":[{"id":56},{"id":57}]
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-8.png",
+          "text":"Enter the house just to the upper right of the Inn. Talk to Kimberly in here to give her Mathiu's Letter. She can't read, so Gremio will read it to her; she thinks about joining you, then after looking at who you're with, she'll decide that she'll only join after she gets a date with Flik! After that fun-filled date, she'll decide to head over to Headquarters. She'll tell you that Tesla is in this town, but he's going by the name of Albert. Kimberly joins!"
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-9.png",
+          "text":"Leave this house and go to the one northwest of it. Talk to 'Albert' here, and Viktor will start interrogating him; Viktor will trick him into saying that he is in fact Tesla the Scrivener. He really doesn't want to go back to his life of crime, but he doesn't have any choice here. Tesla joins the party! Head back out to the World Map and use the Blinking Mirror to go back to HQ."
+        },
+        {
+          "type":"place",
+          "text":"Headquarters",
+          "party":[
+            {"name":"Pahn","level":"28"},
+            {"name":"Viktor","level":"32"},
+            {"name":"Flik","level":"35"},
+            {"name":"Hero","level":"33"},
+            {"name":"Gremio","level":"33"},
+            {"name":"Kai","level":"28"}
+          ]
+        },
+        {
+          "type":"plain",
+          "picture":"19/19-10.png",
+          "text":"Go back to the fourth floor once again and talk to Mathiu. He'll tell you that he had Kimberly and Tesla make forged Imperial papers that will allow you to walk into Soniere Prison. You'll receive the [_FAKE ORDERS_]. Now, prepare for a tough road ahead, and SAVE your game at the Inn. When you're ready, teleport over to Soniere Prison, or walk southwest of Teien if you haven't been there before."
         }
       ]
     });
