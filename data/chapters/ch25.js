017dc54c5c4ae785455de5b3f786471553d3f10a
Mathieu Barreau <mathieu.barreau@list.lu>
2026-08-04 14:35:23 +0200

    chapt 25

--- data/chapters/ch25.js
+++ data/chapters/ch25.js
@@ -1,4 +1,253 @@
 guideData.chapters.push({
-      "id": 25,
-      "title": "Qlon, where the Star Dragon Sword sleeps"
-    });
+  id: 25,
+  title: "Qlon, where the Star Dragon Sword sleeps",
+  picture: "25/25.png",
+  party: [
+    { name: "Hero", level: 39 },
+    { name: "Viktor", level: 38 },
+    { name: "Flik", level: 38 },
+    { name: "Cleo", level: 34 },
+    { name: "Kirkis", level: 38 },
+    { name: "Stallion", level: 38 },
+  ],
+  savepoints: ["Inn"],
+  bits: 5000,
+  items: [
+    "Medicine",
+    "Escape Talisman",
+    "Medicine",
+    "Mega Medicine",
+    "Skill Rune Piece",
+    "Mega Medicine",
+  ],
+  equipment: ["Crimson Cape", "Head Gear", "Silverlet"],
+  runes: ["Flowing Crystal", "Boar Crystal", "Champion's Crystal"],
+  collectibles: [
+    "Old Book Vol. 3",
+    "Window Rune",
+    "Old Book Vol. 2",
+    "War Scroll",
+  ],
+  recruits: [
+    { id: 81 },
+    { id: 82 },
+    { id: 83 },
+    { id: 84 },
+    { id: 85 },
+    { id: 86 },
+    { id: 87 },
+    { id: 88 },
+  ],
+  enemies: ["Banshee", "Clay Doll", "Red Elemental (PS1) / Wisp (Remaster)"],
+  paragraphs: [
+    {
+      type: "plain",
+      picture: "25/25-1.png",
+      text: " Go to the upper right part of the screen and  talk to Zen; you'll automatically give him  the Red, Blue, and Yellow Flower Seeds, if  you brought them with you. (If not, they can  be bought at the Item Shops in the Warrior's  Village, Rikon, and Teien.) Now that he can  finish his work here, he joins your party!",
+    },
+    {
+      type: "plain",
+      picture: "25/25-2.png",
+      text: "Enter the temple through the big door here. Go to the upper right of the temple and talk to Morgan; he's been blind since birth, but he claims that he can see better than most others, so go ahead and recruit him as well! Go to the upper left part and talk to Hugo if you want; he can't seem to find his War Scroll, so we'll let him know if we find it. Check the left bookshelf close to Hugo to find [_OLD BOOK VOL. 3_].",
+    },
+    {
+      type: "plain",
+      picture: "25/25-3.png",
+      text: "Now, talk to the man in front; this is Fukien, and he has foreseen the coming of Hero, since he's the destined leader of the 108 Stars of Heaven and Earth, and everyone who has joined us so far was born under one of these stars.",
+    },
+    {
+      type: "plain",
+      picture: "25/25-4.png",
+      text: "Fukien says that he knows of a way to defeat Neclord, and he'll join your party! He'll take you out to the entrance to the Cave of the Past, and remove the guard to it. Now we must go in to face the past, or something like that.",
+    },
+    {
+      type: "note",
+      text: "Make good use of Kirkis and Stallion's Wild Arrow Attack against groups of enemies in here.",
+    },
+    {
+      type: "plain",
+      picture: "25/25-5.png",
+      text: "Once inside, head north to a fork; to the east is a Journeyman's Crystal, so use it, then go back and head north, then down the stairs. Follow the path to a four-way fork; head west and up the stairs to find a chest with a [_CRIMSON CAPE_], then go back and take the northern path to the east. Follow the path as it curves around to two chests, one with a [_MEDICINE_] inside, and the other with a [_FLOWING CRYSTAL_]. Go back to the fork and take the southern path to the east.",
+    },
+    {
+      type: "plain",
+      picture: "25/25-6.png",
+      text: "On this screen, head straight down and you'll start walking through the wall like we did at  the Toran Castle; head straight down, then take a step or two to the right, then down some more to the next screen, where you'll find the [_WINDOW CRYSTAL_], which is used to recruit Window in the Warrior's Village.",
+    },
+    {
+      type: "plain",
+      text: "Go back through the hidden path, then go up the stairs to the east and get the [_HEAD GEAR_] out of the chest. Head east to another fork; go up first and grab the [_OLD BOOK VOL. 2_] out of the chest, then go back and take the southern path. Move down and left past the stairs and open the chest for [_5000 BITS_]. Now go up the stairs and north, then get the [_ESCAPE TALISMAN_] before going down some more stairs.",
+    },
+    {
+      type: "plain",
+      picture: "25/25-7.png",
+      text: "Head south through this little room, and you'll be in another large room with a hidden path.If you follow the lines like the note below, it will lead you to the magician Crowley. He is the best magician in the game, but he says that he will join you when your castle gets bigger, so we have to wait a little while longer before he joins.",
+    },
+    {
+      type: "note",
+      text: "o-------------------o<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;|&nbsp;###&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;>V&nbsp;###&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;|&nbsp;####&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;|&nbsp;#####&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;V<&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;|&nbsp;#######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;>V&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;|&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;X&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;#############&nbsp;&nbsp;&nbsp;|<br />o-------------------o",
+    },
+    {
+      type: "plain",
+      picture: "25/25-8.png",
+      text: "Go to the middle of this room to a fork; head east and north first to come to a dead-end with the [_WAR SCROLL_] that Hugo in the Temple was looking for, then make your way back to the fork and head southeast.",
+    },
+    {
+      type: "plain",
+      text: "Follow the lengthy path here until you come to a fork where you can either go into a room or continue north to some stairs; go into the room and get the [_MEDICINE_] and [_SILVERLET_] out of there, then go back out and head north down the stairs. Follow the path down here, and when you can either go north into a room or east to the next screen, go north into the room and grab the [_MEGA MEDICINE_] and the [_SKILL RUNE PIECE_].",
+    },
+    {
+      type: "plain",
+      picture: "25/25-9.png",
+      text: "Go back out and head east, then follow the path the rest of the way until you reach the weapon that Fukien told us about; the Star Dragon Sword, and it can talk! It will take you into the past...",
+    },
+    {
+      type: "plain",
+      picture: "25/25-10.png",
+      text: "You'll be in a town with wooden houses; go towards the upper right for a kid to run that way, then get closer to the house up there for the kid to run to the lower left, then go over and talk to him; it's Ted! He'll ask if you've come to take the treasure.",
+    },
+    {
+      type: "choices",
+      items: ["What treasure?", "No."],
+    },
+    {
+      type: "plain",
+      picture: "25/25-11.png",
+      text: "Pick either one, then Ted will be called by his grandpa. Cleo will notice that his grandpa called him Ted and will wonder if it's the same Ted that they know.",
+    },
+    {
+      type: "choices",
+      items: ["He did remind me of Ted.", "No, it must be someone else."],
+    },
+    {
+      type: "plain",
+      picture: "25/25-12.png",
+      text: "Choose either option again, then go to the upper right and talk to the Village Chief; he thinks that you were sent by some woman, but after Viktor insists    that you weren't he'll tell you that you should leave. Go into the house here and open the chest in here for a [_MEGA MEDICINE_].",
+    },
+    {
+      type: "plain",
+      picture: "25/25-13.png",
+      text: "Talk to the Chief again and Lady Windy will show up, with Neclord! Windy will demand from the Chief to give her the Soul Eater, since she knows that this is the Village of the Hidden Rune. Another man named Yuber then decides to set the town ablaze. When Windy tries to take the Soul Eater from the Chief, he will use it on her and flee.",
+    },
+    {
+      type: "plain",
+      picture: "25/25-14.png",
+      text: "In the back of the house, the Ted's grandpa will transfer the Soul Eater over to him, then he will ask you to take Ted and leave while he acts as a decoy. Leave the house and follow the path on the edge of the village to run into Yuber; just as he's about to attack you, Neclord will come in and stop him so they can go chase the Chief in the forest.",
+    },
+    {
+      type: "plain",
+      text: "Your party will conclude that this is 300 years in the past, when Ted acquired the Soul Eater, but now they have to find a way back to the present. Go back into the house to the north and out the back door, then follow the path south and west to a chest containing a [_BOAR CRYSTAL_].",
+    },
+    {
+      type: "plain",
+      picture: "25/25-15.png",
+      text: "Go back and through the house, then go into the house to the south; open the chest in the upper right corner for a [_CHAMPION'S CRYSTAL_]. Now, leave the house and go up the stairs to the light. You'll have a choice about what to do with Ted.",
+    },
+    {
+      type: "choices",
+      items: ["We'll take him along.", "We can't take him along."],
+    },
+    {
+      type: "plain",
+      picture: "25/25-16.png",
+      text: "Choose either option, because in any case, Ted can't leave the past and come with you. Head up the stairs here, and you'll be back in the Qlon caves. Head south back to the  Star Dragon Sword; he'll ask Viktor about his mission, and when Viktor says that he's going to kill Neclord, the Star Dragon Sword decides that he will join Viktor on that mission. Now Viktor's new weapon is the Star Dragon Sword! (if it's already leveled to 12 or higher, then it's the King Dragon Sword.)",
+    },
+    {
+      type: "plain",
+      text: "If you want to, you can walk all the way out of here, which is what I'm going to do. However, you should still have that Escape Talisman that you found earlier, so if you want to, use that to escape to the entrance of Qlon.",
+    },
+    {
+      type: "plain",
+      picture: "25/25-17.png",
+      text: "Go into the Temple and go to the upper left part to talk to Hugo. He'll be glad to see that you've found the War Scroll, and for finding it for him, he'll join your party! Leave Qlon and go back to HQ. It's time to do some more recruiting, so teleport over to Garan.",
+    },
+    {
+      type: "note",
+      text: "Now that Fukien has joined your party, you have access to the a Resurrection Rune. This is the only rune in the game that can be used to revive fallen characters in battle.",
+    },
+    {
+      type: "place",
+      text: "Garan",
+      party: [
+        { name: "Hero", level: 45 },
+        { name: "Viktor", level: 45 },
+        { name: "Flik", level: 45 },
+        { name: "Cleo", level: 45 },
+        { name: "Kirkis", level: 45 },
+        { name: "Stallion", level: 45 },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "25/25-18.png",
+      text: "On the left side of Garan is someone named Quincy; you couldn't recruit Quincy before, but now that 81 Stars of Destiny have joined your party, Quincy will join! After you've done that, head back to HQ.",
+    },
+    {
+      type: "plain",
+      text: "With Kirkis still in your party, teleport over to the Burnt Village of the Elves, then walk northwest to the Kobold Village.",
+    },
+    {
+      type: "place",
+      text: "Great Forest-Kobold Village",
+      party: [
+        { name: "Hero", level: 45 },
+        { name: "Viktor", level: 45 },
+        { name: "Flik", level: 45 },
+        { name: "Cleo", level: 45 },
+        { name: "Kirkis", level: 45 },
+        { name: "Stallion", level: 45 },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "25/25-19.png",
+      text: "Go into the Inn, then talk to Rubi at the top; he will see that Kirkis has become much stronger than he has, and since Kirkis is over level 40, Rubi will join the Liberation Army too! Leave here, then teleport to Teien.",
+    },
+    {
+      type: "place",
+      text: "Teien",
+      party: [
+        { name: "Hero", level: 45 },
+        { name: "Viktor", level: 45 },
+        { name: "Flik", level: 45 },
+        { name: "Cleo", level: 45 },
+        { name: "Kirkis", level: 45 },
+        { name: "Stallion", level: 45 },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "25/25-20.png",
+      text: "Go to the far right part of town and talk to the man beside the water; this is Eikei, a martial arts master. Since you're over level 40, he will decide to lend you his  trength! Now, head back to Headquarters.",
+    },
+    {
+      type:"note",
+      text: "Eikei has a Double-beat Rune equipped on him by default; I recommend taking it off of him and putting it on Viktor."
+    },
+    {
+      type: "plain",
+      text: "Now, I'm going up to the fourth floor and kicking Kirkis and Stallion out of my party and putting Kai in. I suggest leaving the other spot empty since Hix will be joining us soon. When you're ready, teleport back to the Warrior's Village.",
+    },
+    {
+      type: "place",
+      text: "Warrior's Village",
+      party: [
+        { name: "Kai", level: 37 },
+        { name: "Viktor", level: 45 },
+        { name: "Flik", level: 45 },
+        { name: "Cleo", level: 45 },
+        { name: "Hero", level: 45 },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "25/25-21.png",
+      text: "Ignore what's going on just above you, and head over to the Item Shop. Go into the upper right room and talk to the man named Window; since you've found the Window Crystal at Qlon, he can make really good stained glass, a.k.a., your menu window, and will join your party if you give it to him! You can talk to him at HQ to change your window settings depending on which Window Settings you have obtained."
+    },
+    {
+      type: "plain",
+      picture: "25/25-22.png",
+      text: "Now, go back outside and talk to Zorak; Viktor will show him the Star Dragon Sword. While we were gone, Zorak and the village got invitations from Neclord to he and Tengaar's wedding, so if we don't hurry up, Tengaar will be drained of blood! The villagers will leave ahead of us, so after they're gone, go to the Inn and save. When you're ready, head southwest of the Warrior's Village and enter Neclord's Castle."
+    }
+  ],
+});
