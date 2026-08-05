f57147206d713f828693190c44f79e21106b3b0d
Mathieu Barreau <mathieu.barreau@list.lu>
2026-08-05 15:53:58 +0200

    chapt 26

--- data/chapters/ch26.js
+++ data/chapters/ch26.js
@@ -1,4 +1,232 @@
 guideData.chapters.push({
-      "id": 26,
-      "title": "Neclord's Castle, the symphony ends here"
-    });
+  id: 26,
+  title: "Neclord's Castle, the symphony ends here",
+  savepoints: "First area on the left",
+  items: [
+    "Antitoxin",
+    "Medicine",
+    "Antitoxin",
+    "Earth Rune Piece",
+    "Magic Rune Piece",
+  ],
+  equipment: ["Full Helmet", "Earth Boots", "Cape of Darkness"],
+  runes: ["Rage Crystal"],
+  collectibles: ["Sound Set 1", "Green Paint"],
+  recruits: [
+    { id: 89 },
+    { id: 90 },
+    { id: 91 },
+    { id: 92 },
+    { id: 93 },
+    { id: 94 },
+  ],
+  enemies: [
+    "Demon Sorcerer (PS1) / Zombie Sorcerer (Remaster)",
+    "Hell Unicorn (PS1) / Zombie Unicorn (Remaster)",
+    "Larvae (PS1) / Specter (Remaster)",
+    { type: "boss", name: "Neclord (2nd fight)" },
+  ],
+  party: [
+    { name: "Kai", level: "37" },
+    { name: "Viktor", level: "45" },
+    { name: "Flik", level: "45" },
+    { name: "Hero", level: "45" },
+    { name: "Cleo", level: "45" },
+  ],
+  paragraphs: [
+    {
+      type: "plain",
+      picture: "26/26-1.png",
+      text: "Go to the front of the castle and you'll see the villagers getting ready to go into the castle, then Neclord will come out; he tells you that you are the guest of honor, and if you don't make it to the top before sunset, Tengaar will become his bride!",
+    },
+    {
+      type: "plain",
+      picture: "26/26-2.png",
+      text: "You're the only one who can enter, so Zorak begs you to save Tengaar. As you go to enter, Hix comes in, saying that he wants to go and help too, even though Zorak says he's not a warrior yet.",
+    },
+    {
+      type: "choices",
+      items: ["You'll just be in the way.", "All right. Come along."],
+    },
+    {
+      type: "plain",
+      picture: "26/26-3.png",
+      text: "Choose 2, and Hix will get the permission of Zorak and will become a warrior. He will then join your party! Now before continuing on, I recommend going back to Headquarters and sharpening Hix's weapon, as well as upgrading his equipment. The monsters are tough and Neclord isn't exactly easy. If you still have the Clone Rune that you had on Viktor, put it on Hix. I would also recommend putting two Mega Medicines on each character, and if you want to play it REALLY safe, a Sacrificial Buddha on each. Once you're prepared, head back and enter the castle.",
+    },
+    {
+      type: "plain",
+      picture: "26/26-4.png",
+      text: "Go into the room to the right of the stairs and go down the stairs in here to a chest with a [_FULL HELMET_]. You can talk to the Zombie down here for hints, but you have to pay for them. Don't bother wasting your money since I'm free! If you want to save your game, go through the door to the left of the main stairs and go downstairs to the Journeyman's Crystal. When you're ready, back in the first room, head up the stairs to the second floor. Follow the path up here until you make it out to the balcony. Head left, then down back into the castle. Keep following the path until you can go up to the third floor.",
+    },
+    {
+      type: "plain",
+      picture: "26/26-5.png",
+      text: "Up here, go down to the wall, then left and up into a room with a chest containing the [_SOUND SETTING 1_]. Head back and enter the room beside the stairs to get an [_ANTITOXIN_], then enter the room to the lower right of it for a [_MEDICINE_]. Now enter the room on the upper right, and you'll see four paintings; examine the third one from the left (Chestnut Tree) 1st, then the second one (Girl in the Early Afternoon) 2nd, the fourth one (Peasant Girl) 3rd, and the first one (King of the Night) 4th; doing so will open a door out to the balcony. Go out there and head right and down back inside.",
+    },
+    {
+      type: "plain",
+      text: "Go into the room below you for some [_EARTH BOOTS_]. Go into the westernmost room and get the [_GREEN PAINT_], then go through the center room and up to the next floor. Go right and into this room for another [_ANTITOXIN_], then enter the room below that one to find an [_EARTH RUNE PIECE_]. Go through the upper right door of this area to be back out on the balcony again; go left and down back into the castle.",
+    },
+    {
+      type: "plain",
+      text: "Go into the first room on the right and grab the [_CAPE OF DARKNESS_], then go behind it and enter the room above it for a [_MAGIC RUNE PIECE_]. Go to the lower right part of the area and head up these stairs.",
+    },
+    {
+      type: "plain",
+      picture: "26/26-6.png",
+      text: "You'll see Neclord playing on his giant organ, waiting for sunset. After he's finished playing his own wedding music, he will turn around and ask why you took so long. Viktor asks him about the village of North Window, where Viktor came from, and Neclord thinks that he remembers him, but the village doesn't really ring a bell. Viktor draws the Star Dragon Sword, and Hix will draw his sword 'Tengaar', and the battle with Neclord will commence!",
+    },
+    {
+      type: "boss",
+      text: "Neclord (2nd fight)",
+      reward: "10000 Bits",
+      strategy:
+        "FIRST OFF, BEFORE WASTING ANY OF YOUR SPELLS, have Viktor attack with the Star Dragon Sword while everyone else Defends so you will be able to attack him normally. Once you've done that, have Flik and Cleo use the strongest spells on their runes, and when they start to run low on MP, just have them attack/heal. Have Hix use the Clone rune, then heal when  he's unbalanced, and finally, have Hero, Viktor, and Kai just use normal attacks, and heal when necessary. He hits VERY HARD, so be prepared to   have someone heal every round with Mega Medicines. He's tough, but if you keep everyone above 250 HP, you should make it through.",
+    },
+    {
+      type: "plain",
+      picture: "26/26-7.png",
+      text: "The Star Dragon Sword says that he is the reincarnation of the Night Rune, and since Neclord is just a servant of the night, he cannot hurt him. Viktor will take the Star Dragon Sword and strike Neclord down for good! Tengaar has been saved, so it's time to head out of here.",
+    },
+    {
+      type: "note",
+      text: "Before doing ANYTHING, take off anything important on Viktor; he'll be leaving temporarily.",
+    },
+    {
+      type: "plain",
+      text: "Before leaving this place, go behind the staircase; there's a stained glass window to your south, so search the circle at the top of the window to find a [_RAGE CRYSTAL_].",
+    },
+    {
+      type: "plain",
+      picture: "26/26-8.png",
+      text: "Guess what? You can't use an Escape Talisman in this castle, so you now have the fun task of walking ALL THE WAY back down. When you reach the front of the castle, Zorak will be glad to see Tengaar brought to safety. Viktor tells you that since Neclord is gone, he must travel to his village and deliver the news to everyone, but he promises to return to the Liberation Army.",
+    },
+    {
+      type: "choices",
+      items: ["That could be a problem.", "All right. Come back soon."],
+    },
+    {
+      type: "plain",
+      picture: "26/26-9.png",
+      text: "Choose 2 to let him leave. Tengaar says  that she would like to join the Liberation Army with Hix, so when Viktor leaves, Tengaar joins your party, taking his spot in your battle party. Now, leave here and, unless you need something around here, head back to Headquarters.",
+    },
+    {
+      type: "place",
+      text: "Headquarters",
+    },
+    {
+      type: "plain",
+      picture: "26/26-10.png",
+      text: "Before doing anything, get on your boat and leave the castle, then once you're on the World Map, re-enter Headquarters. You will see a scene showing that your castle has gotten bigger yet again; and since the flag is on it this time, that means that it's as big as it can get! Sweet!",
+    },
+    {
+      type: "plain",
+      text: "Head up to the fourth floor and talk to Mathiu. He says that Kasim Hazil has been put in charge of the northern defenses, and he will run the numbers by you about how many troops they have, and we only have enough to take on just Kasim Hazil. Humphrey speaks up, saying that he's an old friend of Joshua, Commander of the Knights of the Dragon's Den, so he will join you so you can go talk to them to see if they'll join.",
+    },
+    {
+      type: "choices",
+      items: ["I'd rather not go.", "Let's go meet these Dragon Knights."],
+    },
+    {
+      type: "plain",
+      text: "Choose option 2, and Flik and Humphrey will join your party so you can go talk to the Dragon Knights. Leave the floor, not just the room, then come back in and talk to Sanchez so you can fill up the rest of your party. (I'm taking Cleo, Eileen, and Valeria, mainly for their Unite Attack, but because Eileen is a decent magic user as well.)",
+    },
+    {
+      type: "plain",
+      text: "Make sure to suit them up with good weapons and armor, and I also put that Rage Crystal on Cleo. Before we go talk to the Dragon Knights, we have a few more people we can recruit. First, head to Rikon.",
+    },
+    {
+      type: "place",
+      text: "Rikon",
+      party: [
+        { name: "Valeria", level: "31" },
+        { name: "Flik", level: "48" },
+        { name: "Humphrey", level: "23" },
+        { name: "Hero", level: "48" },
+        { name: "Cleo", level: "48" },
+        { name: "Eileen", level: "13" },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "26/26-11.png",
+      text: "Head to the Inn. If your castle is at the highest level (meaning that you've seen the scene with a flag on your castle), there should be a man in a black cloak in the upper left part of the room; if there isn't, head back out onto the World Map then come back and check again. If you've been following the guide from the start, you should have seen it already, so just keep on exiting and entering until he shows up. Once he's here, talk to him; this is Clive. Just talk to him and will join your party! Now, the next stop is Kalekka."
+    },
+    {
+      type: "place",
+      text: "Kalekka",
+      party: [
+        { name: "Valeria", level: "31" },
+        { name: "Flik", level: "48" },
+        { name: "Humphrey", level: "23" },
+        { name: "Hero", level: "48" },
+        { name: "Cleo", level: "48" },
+        { name: "Eileen", level: "13" },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "26/26-12.png",
+      text: " Go into the first house to the north if you  are coming in from the south; talk to Leon  inside. He will tell you to tell Mathiu that  he's impressed on how big the Liberation Army  has become."
+    },
+    {
+      type: "plain",
+      picture: "26/26-13.png",
+      text: "Leave Kalekka and head back to Headquarters. Talk to Mathiu on the fourth floor and he will give you a letter to give to Leon. Head back to Kalekka and give Leon the letter; after you give it to him, Mathiu himself will come in and say that he'll do anything to win this war, even if it means asking for his help. Even though Mathiu doesn't trust him, Leon decides that he will help the Liberation Army and fight with Mathiu one last time. Leon joins! Leave Kalekka, then head over to Qlon."
+    },
+    {
+      type: "place",
+      text: "Qlon",
+      party: [
+        { name: "Valeria", level: "34" },
+        { name: "Flik", level: "48" },
+        { name: "Humphrey", level: "33" },
+        { name: "Hero", level: "48" },
+        { name: "Cleo", level: "48" },
+        { name: "Eileen", level: "29" },
+      ],
+    },
+    {
+      type: "plain",
+      text: " Head north past the Temple and into the caves  once again. Go north to the next screen, then  down the stairs to the third screen, then  east and south to the fourth screen. Head  east and then south at the fork on this  screen, then follow the path up a set of stairs then down another set on the next screen. Head south through this little room, and you'll be in the big room where you can find Crowley."
+    },
+    {
+      type: "plain",
+      text: "Head south from the entrance of the big room and search the bottom wall for the entrance of the maze, then follow the diagram below to get to Crowley. Talk to him, and since your castle is now large enough, he will decide to join your party!"
+    },
+    {
+      type: "note",
+      picture: "26/26-14.png",
+      text: "<br />o-------------------o<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;|&nbsp;###&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;>V&nbsp;###&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;|&nbsp;####&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;|&nbsp;#####&nbsp;&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;V<&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;|&nbsp;#######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;###&nbsp;>V&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;|&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;####&nbsp;X&nbsp;######&nbsp;&nbsp;&nbsp;|<br />|&nbsp;&nbsp;&nbsp;#############&nbsp;&nbsp;&nbsp;|<br />o-------------------o",
+    },
+    {
+      type: "plain",
+      text: "All right, we have one more recruit to get before continuing on. Head out of Qlon, then head over to Neclord's Castle."
+    },
+    {
+      type: "place",
+      text: "Neclord's Castle",
+      party: [
+        { name: "Valeria", level: "42" },
+        { name: "Flik", level: "49" },
+        { name: "Humphrey", level: "42" },
+        { name: "Hero", level: "49" },
+        { name: "Cleo", level: "49" },
+        { name: "Eileen", level: "42" },
+      ],
+    },
+    {
+      type: "plain",
+      picture: "26/26-15.png",
+      text: "I shouldn't really have to give you directions, but if you do get lost, check earlier in the guide for directions. Head all the way to the TOP of the castle where you fought Neclord. There is a strange Black Knight here who resembles Yuber; he is Pesmerga, and he is actually pursuing Yuber. He will join your party since you might run into Yuber again!"
+    },
+    {
+      type: "plain",
+      text: "Now march all the way back down, then go back to Headquarters. I switched out my three old members for new ones (Clive, Pesmerga, and Liukan) then upgraded their equipment. You REALLY should bring Liukan with you, since it will save you a trip back and forth to put him in your party in a little while."
+    },
+    {
+      type: "plain",
+      text: "I also put the Spark Rune on Clive since he has a good speed. The Spark Rune makes all of the other members of your party that are slower than the holder of the rune as fast as the holder, and I also put a Ninja Suit instead of the Dragon Armor on him to boost his speed even more. It's very helpful for the slow guys like Pesmerga and Humphrey! I also put Killer Runes on Pesmerga and Humphrey, and a Water Rune on Liukan so he can heal. If you need to prepare for anything else, go ahead, then warp to or travel west-northwest of Antei to the Dragon's Den."
+    }
+  ],
+});
