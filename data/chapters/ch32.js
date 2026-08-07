a946f8e1d67f3a6507382ced81694d720cf5493f
Mathieu Barreau <mathieu.barreau@list.lu>
2026-08-07 14:51:47 +0200

    chapt 32

--- data/chapters/ch32.js
+++ data/chapters/ch32.js
@@ -1,4 +1,133 @@
 guideData.chapters.push({
       "id": 32,
-      "title": "Gregminster, the end of the Scarlet Moon Empire"
+      "title": "Gregminster, the end of the Scarlet Moon Empire",
+      paragraphs:[
+        {
+          type:"plain",
+          text:"Viktor and Flik will join your party, then pick the rest of your party as well. Make sure to stop in at the Item Shop and Armory here in Gregminster to upgrade your equipment. You may also want to go out on the World Map and fight the enemies here for more levels so you're better prepared for Gregminster."
+        },
+        {
+          type:"place",
+          text:"World Map",
+          places:["Gregminster","Lenankamp","Mt. Tigerwolf","Sarady","Rockland","Mt. Seifu","N. Kwaba Area"],
+          enemies:["Ninja Master","Orc","Simurgh"]
+        },
+        {
+          type:"note",
+          text:"The Ninja Master can drop the  Crimson Cape, and the Simurgh can drop the  Thunder Crystal. The Crimson Cape can be used as an accessory on everyone, and has the highest defense EXCEPT for the Gold Necklace that can only be worn by Kobolds."
+        },
+        {
+          type:"plain",
+          text:"Once you're ready, go back into Gregminster to bring and end to this war."
+        },
+        {
+          type:"place",
+          text:"Gregminster",
+          party: [
+              {name:"Kai",level:"60"},
+              {name:"Viktor",level:"58"},
+              {name:"Flik",level:"58"},
+              {name:"Hero",level:"60"},
+              {name:"Tengaar",level:"58"},
+              {name:"Luc",level:"59"}
+          ],
+          enemies:["Colossus","Ekidonna (PS1) / Echidna (Remaster)","Imperial Guards (sword)","Imperial Guards (sabre)","Phantom",{name:"Ain Gide",type:"boss"},{name:"Golden Hydra (right head)",type:"boss"},{name:"Golden Hydra (middle head)",type:"boss"},{name:"Golden Hydra (left head)",type:"boss"}]
+        },
+        {
+          type:"note",
+          text:"The Colossus can drop both Mother Earth Crystals and Power Rings, the Ekidonna can drop Windspun Armor, and the Phantom can drop Earth Boots. The Crimson Capes are better than the Earth Boots in case you wanted better accessories, but the Earth Boots can only be equipped by men."
+        },
+        {
+          type:"plain",
+          picture:"32/32-1.png",
+          text:"Head north toward the palace and you will run into Ain Gide. He's the one who let us through at Kwaba, so Viktor says that he owes him one, and would hate to have to hurt him. Nonetheless, Ain Gide is an Imperial General, and he will fight you to protect the Emperor."
+        },
+        {
+          type:"boss",
+          name:"Ain Gide",
+          reward:"2000 Bits",
+          strategy:"Have Hero use all of his Judgment spells, then attack; have Flik use the Thunder God spell on this Thunder Rune, then use Ball of Lightning; Have Viktor and Kai attack normally, and have Tengaar and Luc cast Shining Wind each round. He's not too difficult if you go all out on him, and he should be done within three rounds."
+        },
+        {
+          type:"plain",
+          text:"After you beat him, I STRONGLY recommend heading back to either HQ or one of the surrounding towns to rest at an Inn to recover your MP, then come back to Gregminster and head north inside the Palace. Once inside, go to the right side of the room and open the chest in the lower right room for the [_WINDSPUN ARMOR_]."
+        },
+        {
+          type:"note",
+          text:"If you will be using this data for Suikoden II, I STRONGLY recommend going back to Headquarters and equipping the Windspun Armor on Humphrey; as far as I know, he's the only character in the game whose armor will carry over; in Suikoden II, his armor is locked so you can't unequip / change it, but if you equip the Windspun Armor on him now, he'll have the Windspun Armor in Suikoden II, which is the best in that game as well. Just thought I'd let you know."
+        },
+        {
+          type:"plain",
+          picture:"32/32-2.png",
+          text:"Follow the carpet up to be attacked by some Imperial Guards, then go up and left, then up the stairs. Follow the path to the Throne Room, then go through the door to the left of the throne and follow this path to the next floor."
+        },
+        {
+          type:"plain",
+          text:"Head right and up to be attacked again, then follow the carpet up to the next screen to be ambushed by five sets of Imperial Guards. After you've taken care of them, go left and up through the doorway to the next screen, then follow the path while being ambushed by five more sets of Guards. Go up the stairs, then through the doorway and east to be ambushed by another group of Guards."
+        },
+        {
+          type:"plain",
+          picture:"32/32-3.png",
+          text:"Don't go north just yet; go east and open the chest for a [_HORNED HELMET_], then go back to the last doorway and head north to a fork. Go west to a room to find a Journeyman's Crystal and a [_POWER RING_]."
+        },
+        {
+          type:"plain",
+          text:"THIS IS THE FINAL SAVE POINT IN THE GAME! SAVE HERE IF YOU WANT TO USE THIS DATA FOR SUIKODEN II !!!"
+        },
+        {
+          type:"plain",
+          picture:"32/32-4.png",
+          text:"After you save, go back to the fork and head east, then up the stairs. Follow the long red carpet to the Gardens on the roof. Head all the way north to meet with Barbarossa. He says that this garden is all that he has left of his Empire, and he will defend it with his own hands. He will use his Dragon King Sword to transform himself into the Golden Hydra!"
+        },
+        {
+          type:"boss",
+          name:"Golden Hydra (right head)",
+        },
+        {
+          type:"boss",
+          name:"Golden Hydra (middle head)",
+        },
+        {
+          type:"boss",
+          name:"Golden Hydra (left head)",
+          strategy:"The Golden Hydra has three different heads to attack. Between Luc and Tengaar, you should have about 10 level 4 spells, so have them both cast  Storm each round, but if your party has taken damage, have one of them cast Shining Wind, alternating between who casts Shining Wind each time,  and your party should stay well healed throughout the battle. Note that the first head (his left, our right) will not take any damage from the Fire/Rage Runes, the second head (his middle) will not take any damage from the Lightning/Thunder Runes, and the third head (his right, our left) will not take any damage from the Wind/Cyclone Runes. Concentrate all of  your attacks on the third head; use Judgment and Thunder God on it, and   when you run out of those, use Black Shadow and Ball of Lightning while having Kai and Viktor attack the third head as well. Once the third head  is gone, concentrate all attacks on the first head, then finally, the middle head. By the time you get to the middle head, you'll probably be using all physical attacks except for Luc and Tengaar. As long as you have Tengaar and Luc alternating with Shining Wind on their Cyclone Runes, you should be pretty safe. Just hope that you get rid of the third head before it starts to heal itself and/or resurrect the other heads as well."
+        },
+        {
+          type:"plain",
+          picture:"32/32-5.png",
+          text:"After you beat the Golden Hydra, the other four former Imperial Generals will come in, then Windy will come in as well. When she goes to try to take your Soul Eater, the souls of Odessa, Gremio, Teo, and Ted will come together and reject Windy. Windy will then attempt to take out Barbarossa, but she can't since the Sovereign Rune on his Dragon King Sword protects him from all magic. Barbarossa will then take Windy, and jump off the top of the Imperial Palace with his love in his arms..."
+        },
+        {
+          type:"plain",
+          picture:"32/32-6.jpg",
+          text:"All of a sudden, the Imperial Palace will start to crumble, so we've got to get out of here quickly! Start making your way back, and on the way, Viktor will volunteer to stall a group of Guards while you escape."
+        },
+        {
+          type:"choices",
+          items:["Don't be ridiculous, Viktor.","Hurry."]
+        },
+        {
+          type:"plain",
+          picture:"32/32-7.jpg",
+          text:"Choose option 2 to let him fight, because he's going to stay anyway. Keep going toward the exit and you'll run into another group.Flik can't let you die, since Odessa chose you to rebuild the country, so he'll stay behind to  fight as well."
+        },
+        {
+          type:"choices",
+          items:["I can't leave you here.","Stay alive, Flik."]
+        },
+        {
+          type:"plain",
+          picture:"32/32-8.jpg",
+          text:"Choose option 2 to go on without him, and you'll exit the Palace while Viktor meets up with Flik to fight the rest of the Imperial Soldiers together!"
+        },
+        {
+          type:"plain",
+          picture:"32/32-9.jpg",
+          text:"After you escape the Palace, everyone will be glad to see that the Empire has been destroyed! In one of the tents, Mathiu is hearing the voices of victory. Liukan tells him that they have indeed won, so now Mathiu can rest peacefully..."
+        },
+        {
+          type:"picture",
+          picture:"32/32-10.jpg"
+        }
+      ]
     });
