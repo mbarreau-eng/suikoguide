ba74c0c0b8364e6751085253e2063935d7545d7c
Mathieu Barreau <mathieu.barreau@list.lu>
2026-08-11 15:42:42 +0200

    prepare S2 hq data

--- S2/data/cities.js
+++ S2/data/cities.js
@@ -1,3 +1,10 @@
 guideData.cities = [
-
+{
+    name: "North Window Village",
+    render:"hq",
+    picture: "",
+    coords:"831,544",
+    stars: "",
+    shops: [],
+  }
 ];
--- S2/data/hq.js
+++ S2/data/hq.js
@@ -1,3 +1,132 @@
 guideData.hq = {
-
+    levels:[],
+    facilities:[
+        
+  {
+    "Facility": "Blacksmith",
+    "Unlocked By": "101",
+    "HQ Level":3,
+    "Availability and Description ": "This facility can upgrade weapons."
+  },
+  {
+    "Facility": "Appraisal Shop",
+    "Unlocked By": "69",
+    "HQ Level":3,
+    "Availability and Description ": "Antiques marked with “?” can be appraised."
+  },
+  {
+    "Facility": "Window Maker",
+    "Unlocked By": "89",
+    "HQ Level":3,
+    "Availability and Description ": "Player can change the design and color of windows here."
+  },
+  {
+    "Facility": "Phonologist",
+    "Unlocked By": "77",
+    "HQ Level":3,
+    "Availability and Description ": "Player can change sound effects here with Sound Sets."
+  },
+  {
+    "Facility": "Cartographer",
+    "Unlocked By": "14",
+    "HQ Level":1,
+    "Availability and Description ": "Player can access the map on the castle wall where it contains information about towns, locations, and buildings."
+  },/*
+  {
+    "Facility": "Trading Post",
+    "Unlocked By": "Gordon",
+    "Availability and Description ": "Unlocks at Castle Level 3  A facility where you can trade goods."
+  },
+  {
+    "Facility": "Teleportation",
+    "Unlocked By": "Viki",
+    "Availability and Description ": "Unlocks at Castle Level 1  Players can teleport to places they've already discovered"
+  },
+  {
+    "Facility": "Farm",
+    "Unlocked By": "Tony",
+    "Availability and Description ": "Unlocks at Castle Level 2  A facility where players can grow and harvest fruits and vegetables."
+  },
+  {
+    "Facility": "Ranch",
+    "Unlocked By": "Yuzu",
+    "Availability and Description ": "Unlocks at Castle Level 2  A facility where you can raise farm animals collected throughout the journey."
+  },
+  {
+    "Facility": "Chinchirorin",
+    "Unlocked By": "Shilo",
+    "Availability and Description ": "Unlocks at Castle Level 2  Players can play the mini-game \"Chinchirorin\". The maximum betting amount is 9900."
+  },
+  {
+    "Facility": "Rune Master",
+    "Unlocked By": "Jeane",
+    "Availability and Description ": "Unlocks at Castle Level 2  Players can interact with this character to manage Runes."
+  },
+  {
+    "Facility": "Library",
+    "Unlocked By": "Emilia",
+    "Availability and Description ": "Unlocks at Castle Level 2  Players can read at this facility."
+  },
+  {
+    "Facility": "Elevator",
+    "Unlocked By": "Adlai",
+    "Availability and Description ": "Unlocks at Castle Level 1  Players can use this to move through floors."
+  },
+  {
+    "Facility": "Inn",
+    "Unlocked By": "Hilda",
+    "Availability and Description ": "Unlocks at Castle Level 1  Players can pay a certain amount to pass a day. This also allows players to save their game."
+  },
+  {
+    "Facility": "Item Shop",
+    "Unlocked By": "Alex",
+    "Availability and Description ": "Unlocks at Castle Level 1  A store that carries miscellaneous items. This shop also carries items discovered at other shops during your travels."
+  },
+  {
+    "Facility": "Detective Agency",
+    "Unlocked By": "Richmond",
+    "Availability and Description ": "Unlocks at Castle Level 1  This character gives information regarding possible recruits that you can get at the moment."
+  },
+  {
+    "Facility": "Restaurant",
+    "Unlocked By": "Hai Yo",
+    "Availability and Description ": "Unlocks at Castle Level 2  Players can play the Cook-Off minigame. Where you earn money depending on how you perform."
+  },
+  {
+    "Facility": "Bath",
+    "Unlocked By": "Tetsu",
+    "Availability and Description ": "Unlocks at Castle Level 1  A bathhouse where players can relax with companions."
+  },
+  {
+    "Facility": "Armor Shop",
+    "Unlocked By": "Hans",
+    "Availability and Description ": "Unlocks at Castle Level 1  An armor shop that carries items discovered at other shops during your travels."
+  },
+  {
+    "Facility": "Gossip",
+    "Unlocked By": "Taki",
+    "Availability and Description ": "Unlocks at Castle Level 1  This character gives information. These can be clues on where to go to progress the story."
+  },
+  {
+    "Facility": "Fishing",
+    "Unlocked By": "Yam Koo",
+    "Availability and Description ": "Unlocks at Castle Level 1  Players can pay 100 potch to start fishing. There will be various fishes to catch."
+  },
+  {
+    "Facility": "Stone Tablet of Promise",
+    "Unlocked By": "Luc",
+    "Availability and Description ": "Unlocks at Castle Level 1  A facility that has existed from the beginning, players can check on their companions here."
+  },
+  {
+    "Facility": "Warehouse",
+    "Unlocked By": "Barbara",
+    "Availability and Description ": "Unlocks at Castle Level 1  Players can deposit and withdraw items here."
+  },
+  {
+    "Facility": "Tavern",
+    "Unlocked By": "Leona",
+    "Availability and Description ": "Unlocks at Castle Level 1  Players can manage their party here."
+  }
+]*/
+    ]
 };
